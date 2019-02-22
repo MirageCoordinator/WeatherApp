4b3f540da13949266f4c9fb97cff167fddfb0471
Dellirium <dellirium@dellirium.ru>
2019-02-22 04:02:13 +0300

    homework

--- app/src/main/java/ru/dellirium/weatherapp/MainActivity.java
+++ app/src/main/java/ru/dellirium/weatherapp/MainActivity.java
@@ -1,7 +1,9 @@
 package ru.dellirium.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MainActivity", "Запуск приложения");
+
+        TextView temperatureText = findViewById(R.id.temperature);
+        String temperature = getString(R.string.temperature);
+        temperature = String.format(temperature, 5);
+        temperatureText.setText(temperature);
+
+
+        TextView humidityText = findViewById(R.id.humidity);
+        String humidity = getString(R.string.humidity);
+        humidity = String.format(humidity, 79);
+        humidityText.setText(humidity);
+
+        TextView cloudinessText = findViewById(R.id.cloudiness);
+        String cloudiness = getString(R.string.cloudiness);
+        cloudiness = String.format(cloudiness, "Высокая");
+        cloudinessText.setText(cloudiness);
+
     }
+
+
 }
