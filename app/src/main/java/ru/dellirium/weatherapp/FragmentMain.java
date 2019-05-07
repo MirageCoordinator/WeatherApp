dfedbaf4c3acb0a691a2d13961e469e9d7b50cd9
Dellirium <dellirium@dellirium.ru>
2019-05-07 16:57:35 +0300

    homework

---
+++ app/src/main/java/ru/dellirium/weatherapp/FragmentMain.java
@@ -0,0 +1,43 @@
+package ru.dellirium.weatherapp;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class FragmentMain extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = getView().findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {      // Обработка нажатий
+            @Override
+            public void onClick(View v) {
+                CheckBox humidityCheckbox = getView().findViewById(R.id.humidityCheckbox);
+                CheckBox cloudinessCheckbox = getView().findViewById(R.id.cloudinessCheckbox);
+                EditText townTextField = getView().findViewById(R.id.editText);
+                Intent intent = new Intent(getActivity(), ActivityWeatherShow.class);
+                intent.putExtra("TownName", townTextField.getText().toString());
+                intent.putExtra("humidityCheckbox", humidityCheckbox.isChecked());
+                intent.putExtra("cloudinessCheckbox", cloudinessCheckbox.isChecked());
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/ru/dellirium/weatherapp/FragmentWeatherShow.java
+++ app/src/main/java/ru/dellirium/weatherapp/FragmentWeatherShow.java
@@ -1,22 +1,29 @@
 package ru.dellirium.weatherapp;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-public class WeatherShow extends AppCompatActivity {
+public class FragmentWeatherShow extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.weather_show);
-        Intent intent = getIntent();
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather_show, container, false);
+    }
 
-        TextView temperatureText = findViewById(R.id.temperature);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Intent intent = getActivity().getIntent();
+
+        TextView temperatureText = getView().findViewById(R.id.temperature);
         String temperature = getString(R.string.temperature);
         temperature = String.format(temperature, 5);
         temperatureText.setText(temperature);
@@ -25,7 +32,7 @@ public class WeatherShow extends AppCompatActivity {
         boolean cloudinessCheckbox = intent.getExtras().getBoolean("cloudinessCheckbox");
 
         if (humidityCheckbox) {
-            TextView humidityText = findViewById(R.id.humidity);
+            TextView humidityText = getView().findViewById(R.id.humidity);
             String humidity = getString(R.string.humidity);
             humidity = String.format(humidity, 79);
             humidityText.setText(humidity);
@@ -33,15 +40,14 @@ public class WeatherShow extends AppCompatActivity {
         }
 
         if (cloudinessCheckbox) {
-            TextView cloudinessText = findViewById(R.id.cloudiness);
+            TextView cloudinessText = getView().findViewById(R.id.cloudiness);
             String cloudiness = getString(R.string.cloudiness);
             cloudiness = String.format(cloudiness, "Высокая");
             cloudinessText.setText(cloudiness);
             cloudinessText.setVisibility(View.VISIBLE);
         }
 
-        TextView townName = findViewById(R.id.town_name);
+        TextView townName = getView().findViewById(R.id.town_name);
         townName.setText(intent.getStringExtra("TownName"));
-
     }
 }
